352f22cd74dbef46ff27413064a8b3968a9c8254
marcosrjjunior <marcosrjjunior@gmail.com>
2019-01-10 22:15:39 +1000

    first commit

---
+++ precache-manifest.19d2f7ed26de7a1638bacaf732cf1e0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f557da9f9e05b129dc18",
+    "url": "/static/js/main.f557da9f.chunk.js"
+  },
+  {
+    "revision": "542c14c2dcba10207838",
+    "url": "/static/js/1.542c14c2.chunk.js"
+  },
+  {
+    "revision": "f557da9f9e05b129dc18",
+    "url": "/static/css/main.9eca2431.chunk.css"
+  },
+  {
+    "revision": "a7683e3ed3f17aef8925a012f79009e6",
+    "url": "/index.html"
+  }
+];
